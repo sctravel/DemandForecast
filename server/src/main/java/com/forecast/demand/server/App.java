119f7b9fe54f87cf90e328cb681be4e379f66a73
MK <Michael@test.com>
2017-08-27 18:58:06 -0700

    adding helloworld handler

--- server/src/main/java/com/forecast/demand/server/App.java
+++ server/src/main/java/com/forecast/demand/server/App.java
@@ -3,7 +3,9 @@ package com.forecast.demand.server;
 
 /**
  * Hello world!
- *
+ * navigate to top level folder which contains pom.xml, then run the following command to
+ * start maven server
+ * mvn clean compile exec:java -e
  */
 public class App 
 {
