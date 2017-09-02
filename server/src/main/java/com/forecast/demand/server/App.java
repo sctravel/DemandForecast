fe7350c19dc09dc4230adc97aabaad9bda706f13
MK <Michael@test.com>
2017-09-01 22:01:24 -0700

    bind restful endpoint to jetty server

--- server/src/main/java/com/forecast/demand/server/App.java
+++ server/src/main/java/com/forecast/demand/server/App.java
@@ -1,5 +1,10 @@
 package com.forecast.demand.server;
 
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
 /**
  * Hello world!
@@ -11,12 +16,35 @@ public class App
 {
     public static void main( String[] args )
     {
-    	jettyServer server = new jettyServer();
+    	
+    	ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+    	Server server = new Server();
+    	server.setHandler(context);
+    	
+    	ServerConnector connector = new ServerConnector(server);
+        server.setHandler(new HelloHandler());
+        connector.setPort(8090);
+        server.setConnectors(new Connector[] {connector});
+        
+        ServletHolder jerseyServlet = context.addServlet(
+                org.glassfish.jersey.servlet.ServletContainer.class, "/*");
+           jerseyServlet.setInitOrder(0);
+
+           // Tells the Jersey Servlet which REST service/class to load.
+           jerseyServlet.setInitParameter(
+              "jersey.config.server.provider.classnames",
+              DataFeeds.class.getCanonicalName());
+       
+      
     	try {
 			server.start();
+			server.join();
+    
 			
 		} catch (Exception e) {
 			e.printStackTrace();
+			server.destroy();
 		}
         System.out.println( "Hello World!" );
     }
