a8b68733bed7f5ec6bd7809ca89c81b0c20cdc82
XI Tu <tuxi1987@gmail.com>
2017-10-02 21:27:42 -0700

    change query response from array to map

--- server/src/main/java/com/forecast/demand/controller/DataFeeds.java
+++ server/src/main/java/com/forecast/demand/controller/DataFeeds.java
@@ -8,9 +8,7 @@ import com.forecast.demand.model.Table;
 import com.forecast.demand.queryGen.SQLQueryGenerator;
 import com.forecast.demand.queryGen.IQueryGenerator;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -56,9 +54,10 @@ public class DataFeeds {
 	@GET
 	@Path("/tables/{tableName}/distinctValues")
     @Produces(MediaType.APPLICATION_JSON+"; charset=utf-8")
-	public Response getDistinctValues(@PathParam("tableName")String tableName, @QueryParam("dimList") List<String> dimList,@QueryParam("dimType") String dimType,@QueryParam("dimLevel") String dimLevel,@QueryParam("Levels") String Levels,
+	public Response getDistinctValues(@PathParam("tableName")String tableName, @QueryParam("dimList") String dimListString,@QueryParam("dimType") String dimType,@QueryParam("dimLevel") String dimLevel,@QueryParam("Levels") String Levels,
 		  @QueryParam("filter") String filter) {
         List<String> columnNames = new ArrayList<>();
+		List<String> dimList = Arrays.asList(dimListString.split(","));
         columnNames.addAll(dimList);
         StringBuilder queryBuilder = new StringBuilder();
         queryBuilder.append(sqlGen.generateSelect(columnNames, true));
@@ -93,9 +92,12 @@ public class DataFeeds {
 	@GET
 	@Path("/tables/{tableName}/query")
 	@Produces(MediaType.APPLICATION_JSON+"; charset=utf-8")
-	public Response query(@PathParam("tableName")String tableName, @QueryParam("dimList") List<String> dimList,
-		  @QueryParam("measureList") List<String> measureList, @QueryParam("filter") String filter) {
+	public Response query(@PathParam("tableName")String tableName, @QueryParam("dimList") String dimListString,
+		  @QueryParam("measureList") String measureListString, @QueryParam("filter") String filter) {
         List<String> columnNames = new ArrayList<>();
+        List<String> dimList = Arrays.asList(dimListString.split(","));
+		List<String> measureList = Arrays.asList(measureListString.split(","));
+
         columnNames.addAll(dimList);
         columnNames.addAll(measureList);
         StringBuilder queryBuilder = new StringBuilder();
@@ -103,7 +105,19 @@ public class DataFeeds {
         queryBuilder.append(sqlGen.generateFrom(tableName));
         if(filter!=null&&!filter.trim().isEmpty()) queryBuilder.append(sqlGen.generateWhere(filter));
         String query = queryBuilder.toString();
-        List<List<String>> res = DBHelper.getQueryResult(query);
+        List<List<String>> queryResult = DBHelper.getQueryResult(query);
+		List<Map<String, String>> res = new ArrayList<Map<String, String>>();
+		for(List<String> list : queryResult) {
+			int idx = 0;
+			Map<String, String> map = new HashMap<>();
+			for(String dim : dimList) {
+				map.put(dim, list.get(idx++));
+			}
+			for(String measure : measureList) {
+				map.put(measure, list.get(idx++));
+			}
+			res.add(map);
+		}
 
         ObjectMapper mapper = new ObjectMapper();
         String value = null;
@@ -120,8 +134,8 @@ public class DataFeeds {
 	@POST
 	@Path("/tables/{tableName}/{DimList}/{changeList}/{filter}")
 	@Produces(MediaType.APPLICATION_JSON+"; charset=utf-8")
-	public String adjustValue(@PathParam("tableName")String tableName, @PathParam("dimList") List<String> dimList,
-		  @PathParam("changeList") List<String> changeList, @PathParam("filter") String filter) {
+	public String adjustValue(@PathParam("tableName")String tableName, @PathParam("dimList") String dimListSrting,
+		  @PathParam("changeList") String changeListString, @PathParam("filter") String filter) {
 	    return "{}";
 	}
 }
