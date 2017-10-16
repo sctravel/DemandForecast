2fbd8829e99d9cc67f28e0c62d1910d422c54eb3
MCisCountingStar <MK@test.com>
2017-10-15 22:00:53 -0700

    auth and authentication checkin

--- server/db/CreateTables.sql
+++ server/db/CreateTables.sql
@@ -28,3 +28,68 @@ CREATE TABLE YumSalesForecast (
     createdTime timestamp default now(),
     lastUpdatedTime timestamp default now() on update now()
 ) ENGINE=InnoDB CHARACTER SET=utf8;
+
+
+
+CREATE TABLE Org
+     (orgId INT PRIMARY KEY AUTO_INCREMENT,
+     orgName VARCHAR(20) NOT NULL,
+     displayName VARCHAR(63) , 
+	 email VARCHAR(255) NOT NULL, 
+     imageIconUrl VARCHAR(63), 
+     creationDatetime TIMESTAMP default now(), 
+     lastUpdatedBy VARCHAR(15) default 'sysuser',
+     lastUpdatedTime TIMESTAMP  DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, 
+    UNIQUE(orgId) 
+     )
+     ENGINE=InnoDB AUTO_INCREMENT=100000001 DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE Users
+     (userId INT PRIMARY KEY AUTO_INCREMENT,
+     orgId INT,
+     roleId INT,
+     email VARCHAR(255) NOT NULL, 
+     passwordHash VARCHAR(63) NOT NULL,
+     firstName VARCHAR(20) NOT NULL,
+     middleName VARCHAR(20) , 
+	 lastName VARCHAR(20) NOT NULL, 
+     displayName VARCHAR(63) , 
+     imageIconUrl VARCHAR(63), 
+     creationDatetime TIMESTAMP default now(), 
+     lastUpdatedBy VARCHAR(15) default 'sysuser',
+     lastUpdatedTime TIMESTAMP  DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, 
+	 FOREIGN KEY (orgId) REFERENCES Org(orgId),
+    UNIQUE(userId) 
+     )
+     ENGINE=InnoDB AUTO_INCREMENT=100000001 DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE Role(
+	roleId INT PRIMARY KEY AUTO_INCREMENT,
+	roleName VARCHAR(255) NOT NULL
+);
+
+
+CREATE TABLE Permission(
+	permissionId INT PRIMARY KEY AUTO_INCREMENT,
+	permissionName VARCHAR(255) NOT NULL
+);
+
+
+CREATE TABLE RolePermission(
+	permissionId INT,
+	roleId INT,
+	FOREIGN KEY (permissionId) REFERENCES Permission(permissionId),
+    FOREIGN KEY (roleId) REFERENCES Role(roleId)
+);
+
+
+
+CREATE TABLE RoleUser(
+	userId INT,
+	roleId INT,
+	FOREIGN KEY (roleId) REFERENCES Role(roleId),
+    FOREIGN KEY (userId) REFERENCES Users(userId)
+);
+
