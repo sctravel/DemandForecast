a11564fb2cc9271038e8f0578546bbc786eb0e4c
XI Tu <tuxi1987@gmail.com>
2017-10-30 21:57:50 -0700

    add userview db operation

--- server/src/main/java/com/forecast/demand/model/UserView.java
+++ server/src/main/java/com/forecast/demand/model/UserView.java
@@ -20,8 +20,48 @@ public class UserView {
     private Date defaultStartDate;
     private Date defaultEndDate;
 
-    public UserView(){
+    public Date getDefaultEndDate() {
+        return defaultEndDate;
+    }
+    public List<String> getDimensions() {
+        return dimensions;
+    }
+    public String getTableName() {
+        return tableName;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public TimeGrain getTimeGrain() {
+        return timeGrain;
+    }
+    public Date getDefaultStartDate() {
+        return defaultStartDate;
+    }
+    public List<String> getMeasures() {
+        return measures;
+    }
 
+    public void setDefaultEndDate(Date defaultEndDate) {
+        this.defaultEndDate = defaultEndDate;
+    }
+    public void setDefaultStartDate(Date defaultStartDate) {
+        this.defaultStartDate = defaultStartDate;
+    }
+    public void setDimensions(List<String> dimensions) {
+        this.dimensions = dimensions;
+    }
+    public void setMeasures(List<String> measures) {
+        this.measures = measures;
+    }
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+    }
+    public void setTimeGrain(TimeGrain timeGrain) {
+        this.timeGrain = timeGrain;
+    }
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public static UserView fromJson(String json) throws Exception {
--- server/src/main/java/com/forecast/demand/util/DbOperation.java
+++ server/src/main/java/com/forecast/demand/util/DbOperation.java
@@ -1,10 +1,29 @@
 package com.forecast.demand.util;
 
+import com.forecast.demand.common.DBHelper;
+import com.forecast.demand.model.UserView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by tuxi1 on 10/23/2017.
  */
 public class DbOperation {
-    public static void createOrUpdateUserView(int userViewId, String tableName, String userViewJson) {
+    public static void createUserView(String userId, String tableName, String userViewJson) {
         String sql = "";
+    }
+
+    public static void updateUserView(int userViewId, String userId, String tableName, String userViewJson) {
+        String sql = "Update UserViews set details = ? where userId = ? and tableName = ? and id = ?" ;
+    }
+
+    public static List<UserView> getUserViews(String userId, String tableName) {
+        List<UserView> userViewList = new ArrayList<>();
+        String sql = "select details from UserViews where userId = ? and tableName = ?";
+
+        //DBHelper.getQueryResult()
+
+        return userViewList;
     }
 }
