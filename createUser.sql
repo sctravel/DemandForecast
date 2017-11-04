765d3c7096256b9630fe374ac8966079866af284
MCisCountingStar <MK@test.com>
2017-11-03 21:24:48 -0700

    db schema and datalod

---
+++ createUser.sql
@@ -0,0 +1,62 @@
+CREATE TABLE Org
+     (orgId INT PRIMARY KEY AUTO_INCREMENT,
+     orgName VARCHAR(20) NOT NULL,
+     displayName VARCHAR(63) , 
+	 email VARCHAR(255) NOT NULL, 
+     imageIconUrl VARCHAR(63), 
+     creationDatetime TIMESTAMP default now(), 
+     lastUpdatedBy VARCHAR(15) default 'sysuser',
+     lastUpdatedTime TIMESTAMP  DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, 
+    UNIQUE(orgId) 
+     )
+     ENGINE=InnoDB AUTO_INCREMENT=100000001 DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE Users
+     (userId INT PRIMARY KEY AUTO_INCREMENT,
+     orgId INT,
+     roleId INT,
+     email VARCHAR(255) NOT NULL, 
+     passwordHash VARCHAR(63) NOT NULL,
+     firstName VARCHAR(20) NOT NULL,
+     middleName VARCHAR(20) , 
+	 lastName VARCHAR(20) NOT NULL, 
+     displayName VARCHAR(63) , 
+     imageIconUrl VARCHAR(63), 
+     creationDatetime TIMESTAMP default now(), 
+     lastUpdatedBy VARCHAR(15) default 'sysuser',
+     lastUpdatedTime TIMESTAMP  DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, 
+	 FOREIGN KEY (orgId) REFERENCES Org(orgId),
+    UNIQUE(userId) 
+     )
+     ENGINE=InnoDB AUTO_INCREMENT=100000001 DEFAULT CHARSET=utf8
+
+DROP TABLE if exists Role;
+CREATE TABLE Role(
+	roleId INT PRIMARY KEY AUTO_INCREMENT,
+	roleName VARCHAR(255) NOT NULL
+);
+
+DROP TABLE if exists Permission;
+CREATE TABLE Permission(
+	permissionId INT PRIMARY KEY AUTO_INCREMENT,
+	permissionName VARCHAR(255) NOT NULL
+);
+
+DROP TABLE if exists RolePermission;
+CREATE TABLE RolePermission(
+	permissionId INT,
+	roleId INT,
+	FOREIGN KEY (permissionId) REFERENCES Permission(permissionId),
+    FOREIGN KEY (roleId) REFERENCES Role(roleId)
+);
+
+
+DROP TABLE if exists RoleUser;
+CREATE TABLE RoleUser(
+	userId INT,
+	roleId INT,
+	FOREIGN KEY (roleId) REFERENCES Role(roleId),
+    FOREIGN KEY (userId) REFERENCES Users(userId)
+);
+
---
+++ dataload.sql
@@ -0,0 +1,81 @@
+INSERT INTO `Inventory`.`Org`
+(`orgId`,
+`orgName`,
+`displayName`,
+`email`,
+`imageIconUrl`,
+`creationDatetime`,
+`lastUpdatedBy`,
+`lastUpdatedTime`)
+VALUES
+(1,
+'test',
+'testOrg',
+'testOrg@test.com',
+'www.test.com',
+ NOW(),
+'admin',
+ NOW());
+ 
+ INSERT INTO `Inventory`.`Permission`
+(`permissionId`,
+`permissionName`)
+VALUES
+(1,
+'admin');
+
+INSERT INTO `Inventory`.`Role`
+(`roleId`,
+`roleName`)
+VALUES
+(1,
+'admin');
+
+
+
+INSERT INTO `Inventory`.`RolePermission`
+(`permissionId`,
+`roleId`)
+VALUES
+(1,
+1);
+
+INSERT INTO `Inventory`.`RoleUser`
+(`userId`,
+`roleId`)
+VALUES
+(1,
+1);
+
+INSERT INTO `Inventory`.`Users`
+(`userId`,
+`orgId`,
+`roleId`,
+`email`,
+`passwordHash`,
+`firstName`,
+`middleName`,
+`lastName`,
+`displayName`,
+`imageIconUrl`,
+`creationDatetime`,
+`lastUpdatedBy`,
+`lastUpdatedTime`)
+VALUES
+(1,
+1,
+1,
+'test@test.com',
+'abcde',
+'Steven',
+'Cat',
+'Currry',
+'Guard',
+'www.nba.com',
+NOW(),
+'admin',
+NOW());
+
+
+
+
